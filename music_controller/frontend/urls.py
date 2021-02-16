8fba00ec7389d4d861ba5116be52c85f6c1cbce3
thetajwar2003 <rahmant7@bxscience.edu>
2021-02-16 14:50:49 -0500

    added two basic routes

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index) # render index template
+    path('', index), # render index template
+    path('join', index),
+    path('create', index)
 ]
