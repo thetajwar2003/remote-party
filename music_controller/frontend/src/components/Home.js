d8ada8b070474cef4fa3dc4ada8f30d4ae0d0b77
thetajwar2003 <rahmant7@bxscience.edu>
2021-02-16 23:37:28 -0500

    added session logic

--- music_controller/frontend/src/components/Home.js
+++ music_controller/frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { Grid, Typography, Button, ButtonGroup } from '@material-ui/core';
 
 import JoinRoom from './JoinRoom';
 import CreateRoom from './CreateRoom';
@@ -8,17 +9,55 @@ import Room from './Room';
 export default class Home extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: null,
+        }
     }
+
+    async componentDidMount() {
+        fetch('/api/user-in-room')
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({
+                    roomCode: data.code
+                }
+                )
+            });
+    }
+
+    home() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" component="h3">
+                        Remote Vibes
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" >
+                        <Button color="primary" to='/join' component={Link}>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to='/create' component={Link}>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
+    }
+
     render() {
         return (
             <Router>
                 <Switch>
-                    <Route exact path="/">
-                        <p>Home page</p>
-                    </Route>
-                    <Route exact path="/join" component={JoinRoom}/>
-                    <Route exact path="/create" component={CreateRoom}/>
-                    <Route path="/room/:roomCode" component={Room}/>
+                    <Route exact path="/" render={() => {
+                        return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`} />
+                        ) : (this.home())
+                    }} />
+                    <Route exact path="/join" component={JoinRoom} />
+                    <Route exact path="/create" component={CreateRoom} />
+                    <Route path="/room/:roomCode" component={Room} />
                 </Switch>
             </Router>
         )
