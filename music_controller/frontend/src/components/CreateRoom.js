f73ec005982875263f7697ae09ad3737e20f1447
thetajwar2003 <rahmant7@bxscience.edu>
2021-02-16 19:09:49 -0500

    styled create-room page

--- music_controller/frontend/src/components/CreateRoom.js
+++ music_controller/frontend/src/components/CreateRoom.js
@@ -1,12 +1,79 @@
 import React, { Component } from 'react'
+import { Button, Grid, Typography, TextField, FormHelperText, FormControl, FormControlLabel, Radio, RadioGroup } from "@material-ui/core"
+import { Link } from 'react-router-dom'
 
 export default class CreateRoom extends Component {
+    defaultVotes = 2;
     constructor(props) {
         super(props);
     }
     render() {
         return (
-            <p>create room</p>
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                     <Typography component='h4' variant='h4'>
+                        Create a Room
+                     </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                     <FormControl component="fieldset">
+                         <FormHelperText>
+                             <div align="center">
+                                 Guest Control of Playback State
+                             </div>
+                         </FormHelperText>
+                         <RadioGroup row defaultValue='true'>
+                             <FormControlLabel 
+                                value="true" 
+                                control={
+                                    <Radio color="primary" />
+                                }
+                                label="Play/Pause"
+                                labelPlacement="bottom"
+                             />
+
+                             <FormControlLabel 
+                                value="false" 
+                                control={
+                                    <Radio color="secondary" />
+                                }
+                                label="No Control"
+                                labelPlacement="bottom"
+                             />
+                         </RadioGroup>
+                     </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl>
+                        <TextField 
+                            required={true} 
+                            type="number" 
+                            defaultValue={this.defaultVotes} 
+                            inputProps={{
+                                min: 1,
+                                style: { 
+                                    textAlign: "center"
+                                }
+                            }}
+                        />
+                        <FormHelperText>
+                            <div align="center">
+                                Votes required to skip song
+                            </div>
+                        </FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="primary" variant="contained">
+                        Create A Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="secondary" variant="contained" to="/" component={Link}>
+                        Back
+                    </Button>
+                </Grid>
+            </Grid>
         )
     }
 }
