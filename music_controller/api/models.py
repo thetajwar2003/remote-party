10100254f1f41bc444f2de31f2d0391e90d1c036
thetajwar2003 <rahmant7@bxscience.edu>
2021-02-19 19:45:29 -0500

    handled votes to skip

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -2,9 +2,10 @@ from django.db import models
 import string
 import random
 
+
 def generate_unique_code():
     length = 6
-    #checks all existing room to make sure that the code generated is truly unique
+    # checks all existing room to make sure that the code generated is truly unique
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
         if Room.objects.filter(code=code).count() == 0:
@@ -12,9 +13,13 @@ def generate_unique_code():
     return code
 
 # Create your models here.
+
+
 class Room(models.Model):
-    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    current_song = models.CharField(max_length=50, null=True)
